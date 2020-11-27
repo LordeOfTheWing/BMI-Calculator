d3060d0a8289cef1c825ddd2db890dd46d03e70b
WekaMbali <klyntonmainya@gmail.com>
2020-11-27 12:07:08 +0300

    Validation of input

--- style.js
+++ style.js
@@ -1,15 +1,23 @@
 const b = document.querySelector('button');
 
 b.addEventListener('click',()=>{
-	let h=document.getElementById('h').value;
+			let h=document.getElementById('h').value;
 			let w=document.getElementById('w').value;
-			
-			if(h <= 0 || w<= 0){
+
+			//Validation to restrict numbers to a reasonable rane
+			if(h <= 0  || w<= 0 ){
 				alert("ERROR!!Please Enter Correct Range Of Values.")
 			}else{
-				let bmi=w/(h/100*h/100);
+			let bmi=w/(h/100*h/100);
 			let bmio=(bmi.toFixed(2));
 
 			document.getElementById("result").innerText="Your BMI is " + bmio;
+
+			//Validation to restrict input values to only numbers
+			if(document.getElementById("result").innerText="Your BMI is " + bmio === "Your BMI is NaN" ){
+				alert("ERROR!You Can Only Input Numbers");
+			}else{
+				document.getElementById("result").innerText="Your BMI is " + bmio;
+			}
 			}		
 });
