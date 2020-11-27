cd4a3867708303e5ded8ad316b6835885b66671a
WekaMbali <klyntonmainya@gmail.com>
2020-11-27 11:28:40 +0300

    Minor Code Fixes

--- style.js
+++ style.js
@@ -1,8 +1,15 @@
-function BMI() {
-			let h=document.getElementById('h').value;
+const b = document.querySelector('button');
+
+b.addEventListener('click',()=>{
+	let h=document.getElementById('h').value;
 			let w=document.getElementById('w').value;
-			let bmi=w/(h/100*h/100);
+			
+			if(h <= 0 || w<= 0){
+				alert("ERROR!!Please Enter Correct Range Of Values.")
+			}else{
+				let bmi=w/(h/100*h/100);
 			let bmio=(bmi.toFixed(2));
 
-			document.getElementById("result").innerHTML="Your BMI is " + bmio;
-		}
+			document.getElementById("result").innerText="Your BMI is " + bmio;
+			}		
+});
